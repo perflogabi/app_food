9995d1a7d8c15478b02dd9802cf567109c93daab
Gabriela Florêncio <perflogabi@gmail.com>
2024-01-25 19:04:51 -0300

    first commit

---
+++ lib/Pages/HomePage.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../Widgets/AppBarWidget.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          // Custom App Bar Widget
+          AppBarWidget(),
+
+          // Search
+          Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 15,
+            ),
+            child: Container (
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0,3),
+                  )
+                ]),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              child: Row(children: [
+                Icon(CupertinoIcons.search,
+                color: Colors.red,
+                ),
+              Container(
+                height: 50,
+                width: 300,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 15,
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: " O que você procura hoje?",
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              Icon(Icons.filter_list),
+            ],),
+            ),
+          )
+        ),
+      ],
+    ),
+  );
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'Pages/HomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      routes: {
+        "/" :(context) => HomePage(),
+      },
+    );  
+  }
+
+}
+
+
+
